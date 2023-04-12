76d8b971baf5e28930933d598f08e2b59350f1b6
Mahabir <mahabirg81@gmail.com>
2023-04-12 11:28:42 +0800

    implemented the method to fetch the data and display the fetched data

---
+++ app.js
@@ -0,0 +1,52 @@
+"use strict:";
+
+searchButton.addEventListener("click", searchWeather); //calling the function searchWeather
+
+function searchWeather() {
+  // to display the loading icon
+  loadingText.style.display = "block";
+  // hide the data while loading
+  weatherBox.style.display = "none";
+  var cityName = searchCity.value;
+  // check if user has not entered anything
+  if (cityName.trim().length == 0) {
+    return alert("Please enter a city name");
+  }
+  // set-up a new HTTP request
+  var http = new XMLHttpRequest();
+  var apiKey = "8d1264f8ebbff5dd371e4f90e86225c9";
+  var url =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=metric&appid=" +
+    apiKey;
+
+  var method = "GET";
+
+  http.open(method, url);
+  http.onreadystatechange = function () {
+    if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+      var data = JSON.parse(http.responseText); // parse my JSON which is a string into a JS object
+      var weatherData = new Weather(
+        cityName,
+        data.weather[0].description.toUpperCase()
+      ); //weather is an array
+      weatherData.temperature = data.main.temp;
+      updateWeather(weatherData);
+    } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+      alert("Something went wrong!");
+    }
+  };
+  http.send();
+}
+
+function updateWeather(weatherData) {
+  weatherCity.textContent = weatherData.cityName;
+  weatherDescription.textContent = weatherData.description;
+  weatherTemperature.textContent = weatherData.temperature;
+
+  // to hide the loading icon when thw data is there
+  loadingText.style.display = "none";
+  // to display the data
+  weatherBox.style.display = "block";
+}
---
+++ weather-data.js
@@ -0,0 +1,16 @@
+"use strict:";
+// setting up constructor function called Weather
+function Weather(cityName, description) {
+  this.cityName = cityName;
+  this.description = description;
+  this._temperature = ""; // this is a private field. it is not passed into the constructor
+} // it is equals to an empty string
+
+Object.defineProperty(Weather.prototype, "temperature", {
+  get: function () {
+    return this._temperature;
+  },
+  set: function (value) {
+    this._temperature = (value * 1.8 + 32).toFixed(2) + "F."; //conversion formula from celcius to fahrenheit and round off to 2 d.p
+  }, // with the unit in Fahrenheit
+}); // configure the property with a JS object
